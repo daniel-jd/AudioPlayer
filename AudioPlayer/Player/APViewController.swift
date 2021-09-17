39c82e172f470c867581d38508213c2c3f4ee50f
Daniel Yarmak <daniel.yarmak@gmail.com>
2021-09-17 16:57:53 +0300

    Initial Commit

---
+++ AudioPlayer/Player/APViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  AudioPlayer
+//
+//  Created by Daniel Yamrak on 17.09.2021.
+//
+
+import UIKit
+
+class APViewController: UIViewController {
+
+    @IBOutlet weak var artistLabel: UILabel!
+    @IBOutlet weak var songTitleLabel: UILabel!
+    @IBOutlet weak var timeFromStartLabel: UILabel!
+    @IBOutlet weak var songDurationLabel: UILabel!
+    @IBOutlet weak var songSlider: UISlider!
+    @IBOutlet weak var volumeSlider: UISlider!
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func menuButtonPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func playButtonPressed(_ sender: Any) {
+    }
+
+    @IBAction func forwardButtonPressed(_ sender: Any) {
+    }
+
+    @IBAction func backwardButtonPressed(_ sender: Any) {
+    }
+
+    @IBAction func volumeSliderMoved(_ sender: Any) {
+    }
+
+    @IBAction func songDurationMoved(_ sender: Any) {
+    }
+}
+
